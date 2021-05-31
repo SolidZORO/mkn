5fd9609230532978600ec8a7d4113ca3b9f3a5f7
Jason Feng <solidzoro@live.com>
2021-05-31 16:35:06 +0800

    test: add a non-css-modules test

--- src/page-components/test/Test/Test.tsx
+++ src/page-components/test/Test/Test.tsx
@@ -6,6 +6,24 @@ import { HtmlMeta, PageContainer } from '@/components';
 
 import styles from './styles.module.less';
 
+// ❌ is not work
+// Please use require
+// import './non-module-less.less';
+
+// ✅ is work!!!
+require('./non-module-less.less');
+
+// ❌ is not work
+// Please move all global CSS imports to src/pages/_app.tsx.
+// Or convert the import to Component-Level CSS (CSS Modules).
+// Read more: https://nextjs.org/docs/messages/css-global
+// Location: src/page-components/test/Test/Test.tsx
+// import './non-module-css.css';
+
+// ❌ is not work
+// Same as 👆 above
+// require('./non-module-css.css');
+
 interface IProps extends IPageProps {
   className?: string;
   style?: React.CSSProperties;
@@ -17,5 +35,11 @@ export const Test: React.FC<IProps> = (props) => (
     <HtmlMeta title="Test" />
 
     <div className={styles['image-bg']}>IMAGE-BG</div>
+
+    <br />
+    <br />
+
+    <div className="test-non-module-less">TEST-NON-MODULE LESS</div>
+    <div className="test-non-module-css">TEST-NON-MODULE CSS</div>
   </PageContainer>
 );
