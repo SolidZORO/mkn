e494bfdc88a50054e29b09347f5d4eea7831a5cb
Jason Feng <solidzoro@live.com>
2021-07-01 15:36:14 +0800

    refactor: update deps

--- next.config.js
+++ next.config.js
@@ -2,17 +2,17 @@
 const withAntdLess = require('next-plugin-antd-less');
 
 module.exports = withAntdLess({
-  modifyVars: {
-    '@THEME--DARK': 'theme-dark',
-  },
+  // modifyVars: {
+  //   '@THEME--DARK': 'theme-dark',
+  // },
   lessVarsFilePath: './src/styles/variables.less',
-  cssLoaderOptions: {
-    esModule: false,
-    sourceMap: false,
-    modules: {
-      mode: 'local',
-    },
-  },
+  // cssLoaderOptions: {
+  //   esModule: false,
+  //   sourceMap: false,
+  //   modules: {
+  //     mode: 'local',
+  //   },
+  // },
 
   // Other NextConfig Here...
   webpack(config) {
