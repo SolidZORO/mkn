fcc4908a3cceabf6f9fd3c10809dd864da32acf4
Jason Feng <solidzoro@live.com>
2021-08-20 11:36:11 +0800

    chore: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 module.exports = {
   extends: ['airbnb-typescript', 'prettier'],
   plugins: ['react', '@typescript-eslint'],
@@ -12,17 +13,45 @@ module.exports = {
   },
   env: {
     browser: true,
-    es6: true,
+    es2021: true,
     jest: true,
   },
   globals: {
     __DEV__: true,
     __PROD__: true,
   },
+  settings: {
+    'import/extensions': ['.ts', '.tsx', '.js', '.jsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    },
+  },
+  //
+  // 0 (off) / 1 (warn) / 2 (error)
   rules: {
     'max-len': [2, 80],
     'no-console': 0,
     'import/prefer-default-export': 0,
+    // 'import/no-mutable-exports': 1, // for mobx displayName by `babel-plugin-add-react-displayname`
+    'import/extensions': 0,
+    'import/order': [
+      2,
+      {
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+      },
+    ],
     'no-underscore-dangle': 0,
     'arrow-body-style': 0,
     'no-restricted-syntax': [
